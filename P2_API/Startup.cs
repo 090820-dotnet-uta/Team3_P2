acb6e41f7200b661da6e77411c27a9cae7b9e0e2
David Dowd <ddowd97@gmail.com>
2020-10-15 09:16:19 -0500

    Updated CORS

--- P2_API/Startup.cs
+++ P2_API/Startup.cs
@@ -27,6 +27,15 @@ namespace P2_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options => // NEW!
+            {
+                options.AddDefaultPolicy(
+                                  builder =>
+                                  {
+                                      builder.WithOrigins("http://localhost:4200/",
+                                                          "https://localhost:5001/").AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
+                                  });
+            });
             services.AddDbContext<P2Context>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
         }
@@ -42,6 +51,8 @@ namespace P2_API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(); // NEW!
 
             app.UseAuthorization();
 
