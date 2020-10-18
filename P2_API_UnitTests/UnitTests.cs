9c3aeaffa6916f9cc485dc974c44b8ba06b56cd5
ChristopherKeller94 <chriskeller94@gmail.com>
2020-10-18 15:08:46 -0500

    added unit test

--- P2_API_UnitTests/UnitTests.cs
+++ P2_API_UnitTests/UnitTests.cs
@@ -43,7 +43,7 @@ namespace P2_API_UnitTests
 
         }
         [Fact]
-        public void TestGetUser()
+        public async void TestGetUser()
         {
             var options = new DbContextOptionsBuilder<P2Context>().UseInMemoryDatabase(databaseName: "TestMethod2").Options;
             using (var context = new P2Context(options))
@@ -61,11 +61,40 @@ namespace P2_API_UnitTests
                 context.SaveChanges();
 
                 int id = 1;
+                var result = await _usersController.GetUserAsync(id);
+                User testUser = result.Value;
+
+                Assert.Equal("c", testUser.City);
+            }
 
 
-                Assert.Equal(1, id);
+        }
+        [Fact]
+        public async void TestPutUser()
+        {
+            var options = new DbContextOptionsBuilder<P2Context>().UseInMemoryDatabase(databaseName: "TestMethod3").Options;
+            using (var context = new P2Context(options))
+            {
+                _usersController = new UsersController(context);
 
 
+                Preferences prefs = new Preferences { Animals = true, PreferencesId = 1, Art = true, Beauty = false, Entertainment = true, Fitness = false, HomeDecour = true, Learning = false, Nightlife = true, Religion = true, Shopping = false };
+                User user1 = new User { City = "c", Email = "e", Password = "p", PreferencesId = 1, PreferencesModel = prefs };
+                User user2 = new User { City = "ci", Email = "em", Password = "pa", PreferencesId = 2, PreferencesModel = prefs };
+                User user3 = new User { City = "cit", Email = "ema", Password = "pas", PreferencesId = 3, PreferencesModel = prefs };
+                context.Users.Add(user1);
+                context.Users.Add(user2);
+                context.Users.Add(user3);
+                context.SaveChanges();
+
+                user1.City = "a";
+                user1.Password = "b";
+
+                var result = await _usersController.UpdateUserAsync(user1);
+                var test = context.Users.Find(1);
+
+                Assert.Equal("a", test.City);
+                Assert.Equal("b", test.Password);
 
             }
 
