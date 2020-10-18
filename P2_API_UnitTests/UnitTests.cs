f2dc67f68320e3b901f18d068a20a9512e0640eb
ChristopherKeller94 <chriskeller94@gmail.com>
2020-10-18 15:23:35 -0500

    added unit tests (2)

--- P2_API_UnitTests/UnitTests.cs
+++ P2_API_UnitTests/UnitTests.cs
@@ -100,5 +100,60 @@ namespace P2_API_UnitTests
 
 
         }
+        [Fact]
+        public async void TestGetUserPrefs()
+        {
+            var options = new DbContextOptionsBuilder<P2Context>().UseInMemoryDatabase(databaseName: "TestMethod4").Options;
+            using (var context = new P2Context(options))
+            {
+                _usersController = new UsersController(context);
+
+
+                Preferences prefs = new Preferences { Animals = true, PreferencesId = 1, Art = true, Beauty = false, Entertainment = true, Fitness = false, HomeDecour = true, Learning = false, Nightlife = true, Religion = true, Shopping = false };
+                User user1 = new User { City = "c", Email = "e", Password = "p", PreferencesId = 1, PreferencesModel = prefs };
+                User user2 = new User { City = "ci", Email = "em", Password = "pa", PreferencesId = 2, PreferencesModel = prefs };
+                User user3 = new User { City = "cit", Email = "ema", Password = "pas", PreferencesId = 3, PreferencesModel = prefs };
+                context.Users.Add(user1);
+                context.Users.Add(user2);
+                context.Users.Add(user3);
+                context.SaveChanges();
+
+                var response = await _usersController.GetUserPreferencesAsync(1);
+                Preferences result = response.Value;
+
+                Assert.True(result.Art);
+                Assert.True(result.HomeDecour);
+                Assert.True(result.Religion);
+
+            }
+
+        }
+        [Fact]
+        public async void TestPostUser()
+        {
+            var options = new DbContextOptionsBuilder<P2Context>().UseInMemoryDatabase(databaseName: "TestMethod5").Options;
+            using (var context = new P2Context(options))
+            {
+                _usersController = new UsersController(context);
+
+
+                Preferences prefs = new Preferences { Animals = true, PreferencesId = 1, Art = true, Beauty = false, Entertainment = true, Fitness = false, HomeDecour = true, Learning = false, Nightlife = true, Religion = true, Shopping = false };
+                User user1 = new User { City = "c", Email = "e", Password = "p", PreferencesId = 1, PreferencesModel = prefs };
+                User user2 = new User { City = "ci", Email = "em", Password = "pa", PreferencesId = 2, PreferencesModel = prefs };
+
+                context.Users.Add(user1);
+                context.Users.Add(user2);
+                context.SaveChanges();
+
+                User user3 = new User { City = "cit", Email = "ema", Password = "pas", PreferencesId = 3, PreferencesModel = prefs };
+                var response = await _usersController.CreateUserAsync(user3);
+
+                User result = context.Users.Find(3);
+
+                Assert.Equal("cit", result.City);
+                Assert.Equal("ema", result.Email);
+            }
+
+        }
     }
 }
